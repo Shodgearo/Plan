5696ae27b5e7b17252402d9cd1c56abd5e370c62
Shodgearo <shodkvest@gmail.com>
2018-05-18 13:28:30 +0600

    Standart tools

--- src/Plan.java
+++ src/Plan.java
@@ -1,8 +1,27 @@
 // ������� ����� ��� ��������-������������
-// Главный класс. Проверка связи
+// Главный класс планировщика.
 
-public class Plan {
+import javax.swing.*;
+import java.awt.*;
+
+public class Plan extends JFrame {
     public static void main(String[] args) {
+        new Plan();
+    }
 
+    final static int WIDTH_FRAME = 550;
+    final static int HIEGHT_FRAME = 400;
+
+    private Plan() {
+        initFrame();
+    }
+
+    private void initFrame() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(WIDTH_FRAME, HIEGHT_FRAME));
+        setTitle("Планировщик задач.");
+        pack();
+        setLocation(this.WIDTH + WIDTH_FRAME, this.HEIGHT + HIEGHT_FRAME);
+        setVisible(true);
     }
 }
