1fe121ab54d7e3b303c29c813b026972001ec6d9
Shodgearo <shodkvest@gmail.com>
2018-05-18 13:45:00 +0600

    Small edits

--- src/Plan.java
+++ src/Plan.java
@@ -10,7 +10,7 @@ public class Plan extends JFrame {
     }
 
     final static int WIDTH_FRAME = 550;
-    final static int HIEGHT_FRAME = 400;
+    final static int HEIGHT_FRAME = 400;
 
     private Plan() {
         initFrame();
@@ -18,10 +18,10 @@ public class Plan extends JFrame {
 
     private void initFrame() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new Dimension(WIDTH_FRAME, HIEGHT_FRAME));
+        setPreferredSize(new Dimension(WIDTH_FRAME, HEIGHT_FRAME));
         setTitle("Планировщик задач.");
         pack();
-        setLocation(this.WIDTH + WIDTH_FRAME, this.HEIGHT + HIEGHT_FRAME);
+        setLocationRelativeTo(null);
         setVisible(true);
     }
 }
