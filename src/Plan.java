fb7d9f8126f93afd42918f1eea27f98fd8772c7d
Shodgearo <shodkvest@gmail.com>
2018-05-18 17:08:56 +0600

    Add listener for test button

--- src/Plan.java
+++ src/Plan.java
@@ -3,6 +3,8 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Plan extends JFrame {
     public static void main(String[] args) {
@@ -30,6 +32,7 @@ public class Plan extends JFrame {
 
     private void initButton() {
         bAdd = new JButton("Добавить");
+        bAdd.addActionListener(new ButtonListener());
     }
 
     private void adding() {
@@ -54,5 +57,12 @@ public class Plan extends JFrame {
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
+    }
+
+    private class ButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            bAdd.setVisible(false);
+        }
     }
 }
