fc0bc7ce6ef8fe11cd53667e7e55e9243a7b178a
Shodgearo <shodkvest@gmail.com>
2018-05-23 12:38:18 +0600

    Add new constructor for more option creating panels

--- src/Panel4Plan.java
+++ src/Panel4Plan.java
@@ -4,7 +4,30 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Panel4Plan extends JPanel {
-    public Panel4Plan(int w, int h) {
-        setPreferredSize(new Dimension(w, h));
+    private JPanel panel;
+    private final int wButton = 90;
+    private final int hButton = 90;
+
+    public Panel4Plan(FlowLayout lm, int w, int h) {
+        setLayout(lm); // Установить деспетчер компановки
+        setPreferredSize(new Dimension(w, h)); // Установить предпочтительные размеры окна
+
+        toolPanel();
+        adding();
+    }
+
+    public Panel4Plan(GridLayout gm, int w, int h) {
+        setLayout(gm); // Установить деспетчер компановки
+        setPreferredSize(new Dimension(w, h)); // Установить предпочтительные размеры окна
+    }
+
+    private void adding() {
+        add(panel);
+    }
+
+    private void toolPanel() {
+        panel = new JPanel();
+        panel.setBackground(Color.BLUE);
+        panel.setPreferredSize(new Dimension(100, 110));
     }
 }
