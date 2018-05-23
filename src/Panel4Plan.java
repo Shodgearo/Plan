41bbda9b94e9ae57a505ee62db745f73dd6a6951
Shodgearo <shodkvest@gmail.com>
2018-05-23 14:18:42 +0600

    Add button on panel for adding task

--- src/Panel4Plan.java
+++ src/Panel4Plan.java
@@ -7,12 +7,14 @@ public class Panel4Plan extends JPanel {
     private JPanel panel;
     private final int wButton = 90;
     private final int hButton = 90;
+    private Button4Add buttonAdd;
 
     public Panel4Plan(FlowLayout lm, int w, int h) {
         setLayout(lm); // Установить деспетчер компановки
         setPreferredSize(new Dimension(w, h)); // Установить предпочтительные размеры окна
 
         toolPanel();
+        initButton();
         adding();
     }
 
@@ -21,13 +23,18 @@ public class Panel4Plan extends JPanel {
         setPreferredSize(new Dimension(w, h)); // Установить предпочтительные размеры окна
     }
 
-    private void adding() {
-        add(panel);
+    private void initButton() {
+        buttonAdd = new Button4Add(wButton, hButton);
     }
 
     private void toolPanel() {
         panel = new JPanel();
         panel.setBackground(Color.BLUE);
-        panel.setPreferredSize(new Dimension(100, 110));
+        panel.setPreferredSize(new Dimension(140, 120));
+    }
+
+    private void adding() {
+        add(panel);
+        panel.add(buttonAdd);
     }
 }
