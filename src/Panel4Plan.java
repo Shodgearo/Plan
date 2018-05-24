bf36ad3e2b5002664bf5df3b386b9c08ff02ca5e
Shodgearo <shodkvest@gmail.com>
2018-05-24 12:11:41 +0600

    Adding listener for button and localizing panel with button and label on center panel up level

--- src/Panel4Plan.java
+++ src/Panel4Plan.java
@@ -2,20 +2,24 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Panel4Plan extends JPanel {
     private JPanel panel;
     private final int wButton = 90;
     private final int hButton = 90;
+    private final int wPanel = 140;
+    private final int hPanel = 140;
     private Button4Add buttonAdd;
     private JLabel adding;
 
     public Panel4Plan(FlowLayout lm, int w, int h) {
-        setLayout(lm); // Установить деспетчер компановки
+        setLayout(null); // Установить деспетчер компановки
         setPreferredSize(new Dimension(w, h)); // Установить предпочтительные размеры окна
 
         initLabel();
-        toolPanel();
+        toolPanel(w, h);
         initButton();
         adding();
     }
@@ -27,17 +31,26 @@ public class Panel4Plan extends JPanel {
 
     private void initButton() {
         buttonAdd = new Button4Add(wButton, hButton);
+        buttonAdd.addActionListener(new ButtonListener());
     }
 
-    private void toolPanel() {
+    private void toolPanel(int w, int h) {
         panel = new JPanel();
+        panel.setBounds((w / 2) - wPanel / 2, (h / 2) - hPanel / 2, wPanel, hPanel);
         panel.setBackground(Color.ORANGE);
-        panel.setPreferredSize(new Dimension(140, 140));
+        panel.setPreferredSize(new Dimension(wPanel, hPanel));
     }
 
     private void adding() {
         add(panel);
         panel.add(adding);
         panel.add(buttonAdd);
+    }
+
+    private class ButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            panel.setVisible(false);
+        }
     }
 }
