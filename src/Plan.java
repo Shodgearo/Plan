fd3b60a03a4661cf53143ab110aebb21a6dd3e56
Shodgearo <shodkvest@gmail.com>
2018-05-24 11:45:52 +0600

    Delete excess import library

--- src/Plan.java
+++ src/Plan.java
@@ -3,8 +3,6 @@
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Plan extends JFrame {
     public static void main(String[] args) {
