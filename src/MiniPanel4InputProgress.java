0269c9705f3e35c82d9cb446de3b93abb5ff0538
Shodgearo <shodkvest@gmail.com>
2018-05-29 12:54:20 +0600

    Standard tools

--- src/MiniPanel4InputProgress.java
+++ src/MiniPanel4InputProgress.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+// Небольшой класс который при выборе радио кнопки будет создавать панельку с одним или двумя полями
 
-public class MiniPanel4InputProgress {
+import javax.swing.*;
+
+public class MiniPanel4InputProgress extends JPanel {
+    private JTextField startField, endField;
+
+    public MiniPanel4InputProgress() {
+        initTextFields();
+
+        setVisible(true);
+    }
+
+    public MiniPanel4InputProgress(String msg) {
+        initTextFields(msg);
+
+        setVisible(true);
+    }
+
+    private void initTextFields(String msg) {
+        startField = new JTextField(msg, 10);
+        startField.setToolTipText("Прогресс");
+    }
+
+    private void initTextFields() {
+        startField = new JTextField("От", 5);
+        startField.setToolTipText("Начало");
+        endField = new JTextField("До", 8);
+        endField.setToolTipText("Конец");
+    }
 }
