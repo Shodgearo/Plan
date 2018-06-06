6a83f3b5adcf772c63e9d10824a0ae570ecdbc43
Shodgearo <shodkvest@gmail.com>
2018-06-06 14:22:42 +0600

    Refactoring panel out task

--- src/Panel4OutTasks.java
+++ src/Panel4OutTasks.java
@@ -4,8 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Panel4OutTasks extends JPanel {
-    public Panel4OutTasks(LayoutManager layout, int w, int h) {
-        setLayout(layout);
+    public Panel4OutTasks(int w, int h) {
+        setLayout(new GridLayout());
         setPreferredSize(new Dimension(w, h));
     }
 }
