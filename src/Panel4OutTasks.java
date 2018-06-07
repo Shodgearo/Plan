53c6bc0c97d8dee398cf19fb1e9fc6bb8befd55e
Shodgearo <shodkvest@gmail.com>
2018-06-07 13:28:38 +0600

    Option for add new task

--- src/Panel4OutTasks.java
+++ src/Panel4OutTasks.java
@@ -2,10 +2,19 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.LinkedList;
 
 public class Panel4OutTasks extends JPanel {
+    private LinkedList<JPanel> listOfTasks;
+
     public Panel4OutTasks(int w, int h) {
         setLayout(new GridLayout());
         setPreferredSize(new Dimension(w, h));
+
+        listOfTasks = new LinkedList<>();
+    }
+
+    public void addNewTask(String commentArea, String typeTask, String begin, String end) {
+
     }
 }
