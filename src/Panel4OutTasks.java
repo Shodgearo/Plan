75dad90477a95064c0d78dec5c49f65bbcfe7550
Shodgearo <shodkvest@gmail.com>
2018-06-07 15:32:29 +0600

    For create needed panel on out tasks panel

--- src/Panel4OutTasks.java
+++ src/Panel4OutTasks.java
@@ -6,15 +6,23 @@ import java.util.LinkedList;
 
 public class Panel4OutTasks extends JPanel {
     private LinkedList<NewTask> listOfTasks;
+    private int countTasks;
 
     public Panel4OutTasks(int w, int h) {
-        setLayout(new GridLayout());
+        setLayout(new GridLayout(0, 1, 0, 20));
         setPreferredSize(new Dimension(w, h));
 
         listOfTasks = new LinkedList<>();
+        countTasks = 0;
     }
 
     public void addNewTask(String commentArea, String typeTask, String begin, String end) {
-        listOfTasks.add(new NewTask(commentArea, typeTask, begin, end));
+        NewTask task;
+
+        if (!end.equals("0")) task = new NewTask(commentArea, typeTask, begin, end, countTasks);
+        else task = new NewTask(commentArea, typeTask, begin, countTasks);
+
+        listOfTasks.add(task);
+        countTasks++;
     }
 }
