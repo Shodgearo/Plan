902aabb3904679d6bfc2d766b9e562deb81cf881
Shodgearo <shodkvest@gmail.com>
2018-06-07 13:32:26 +0600

    Create constructor for panel new task

--- src/NewTask.java
+++ src/NewTask.java
@@ -1,2 +1,9 @@
-public class NewTask {
+// Для добавления новой панели и её настройки
+
+import javax.swing.*;
+
+public class NewTask extends JPanel {
+    public NewTask(String commentArea, String typeTask, String begin, String end) {
+        
+    }
 }
