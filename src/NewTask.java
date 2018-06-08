b580f268c879b7f7b8fe25ba644b4e755fd43bf4
Shodgearo <shodkvest@gmail.com>
2018-06-08 17:07:43 +0600

    Add grid layout

--- src/NewTask.java
+++ src/NewTask.java
@@ -1,6 +1,7 @@
 // Для добавления новой панели и её настройки
 
 import javax.swing.*;
+import java.awt.*;
 
 public class NewTask extends JPanel {
     private String commentArea, typeTask, begin, end, nameTask;
@@ -9,6 +10,7 @@ public class NewTask extends JPanel {
     private JLabel name, start, finish;
 
     public NewTask(String nameTask, String commentArea, String typeTask, String begin, String end, int countTasks) {
+        setLayout(new GridLayout(5, 1));
         this.commentArea = commentArea;
         this.typeTask = typeTask;
         this.begin = begin;
@@ -22,12 +24,11 @@ public class NewTask extends JPanel {
     }
 
     public NewTask(String nameTask, String commentArea, String typeTask, String begin, int countTasks) {
+        setLayout(new GridLayout(5, 1));
         this.commentArea = commentArea;
         this.typeTask = typeTask;
         this.begin = begin;
         this.nameTask = nameTask;
-
-        System.out.println(begin);
 
         id = countTasks;
 
