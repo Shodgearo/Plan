6637e63ade2256a5b5fb14ec308671d8e5254f9b
Shodgearo <Shodkvest@gmail.com>
2018-06-12 22:23:25 +0600

    Change layout for new task.Now grid bag layout.

--- src/NewTask.java
+++ src/NewTask.java
@@ -8,13 +8,15 @@ import java.awt.event.ActionListener;
 public class NewTask extends JPanel {
     private String commentArea, typeTask, begin, end, nameTask;
     private int id;
-//    private JTextField start, finish;
+    private JTextField startField, finishField;
     private JLabel name, start, finish;
     private Frame4Comment frame4Comment;
     private JButton commentButton;
+    private GridBagConstraints c; // Для менеджера компановки
 
     public NewTask(String nameTask, String commentArea, String typeTask, String begin, String end, int countTasks) {
-        setLayout(new GridLayout(5, 1));
+        setLayout(new GridBagLayout());
+        c = new GridBagConstraints();
         this.commentArea = commentArea;
         this.typeTask = typeTask;
         this.begin = begin;
@@ -23,35 +25,43 @@ public class NewTask extends JPanel {
 
         id = countTasks;
 
-        setPreferredSize(new Dimension(120, 80));
+        setPreferredSize(new Dimension(140, 100));
         initLabel();
+        initTextFields();
         initButton();
-        adding();
+        toolsTable();
     }
 
     public NewTask(String nameTask, String commentArea, String typeTask, String begin, int countTasks) {
-        setLayout(new GridLayout(5, 1));
+        setLayout(new GridBagLayout());
+        c = new GridBagConstraints();
         this.commentArea = commentArea;
         this.typeTask = typeTask;
         this.begin = begin;
+        this.end = null;
         this.nameTask = nameTask;
 
         id = countTasks;
 
-        setPreferredSize(new Dimension(120, 80));
+        setPreferredSize(new Dimension(140, 100));
         initLabel();
+        initTextFields();
         initButton();
-        adding();
+        toolsTable();
     }
+
 
     private void initLabel() {
         name = new JLabel(nameTask);
-        start = new JLabel(begin);
-        finish = new JLabel(end);
-
         name.setHorizontalAlignment(SwingConstants.CENTER);
-        start.setHorizontalAlignment(SwingConstants.CENTER);
-        finish.setHorizontalAlignment(SwingConstants.CENTER);
+    }
+
+    private void initTextFields() {
+        startField = new JTextField(begin);
+        finishField = new JTextField(end);
+
+        startField.setHorizontalAlignment(SwingConstants.CENTER);
+        finishField.setHorizontalAlignment(SwingConstants.CENTER);
     }
 
     private void initButton() {
@@ -59,13 +69,6 @@ public class NewTask extends JPanel {
         commentButton.setFont(Panel4Plan.getGenericFont(13));
 
         commentButton.addActionListener(new ButtonListener());
-    }
-
-    private void adding() {
-        add(name);
-        add(start);
-        add(finish);
-        add(commentButton);
     }
 
     public String getName() {
@@ -79,5 +82,34 @@ public class NewTask extends JPanel {
             frame4Comment.setTextComment(commentArea);
             frame4Comment.setEditing();
         }
+    }
+
+    // Настроки для табличного менеджера копановки. (1, 3, 3, 1, 1 - Компоненты строках)
+    private void toolsTable() {
+        // Настрока для имени задачи
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 0;
+        add(name, c);
+
+        // Настройка для поля "от"
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 1;
+        add(startField, c);
+
+        // Настройка для поля "до"
+        if(end != null) { // Только если это не одна задача или кнгиа
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.gridx = 0;
+            c.gridy = 2;
+            add(finishField, c);
+        }
+
+        // Настройка для кнопки комментария
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 3;
+        add(commentButton, c);
     }
 }
